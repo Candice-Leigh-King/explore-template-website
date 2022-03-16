263ff6a0b8ebae9a7c4f1a5d7a10be232a989076
Candice-Leigh-King <candyschofie@gmail.com>
2022-03-16 10:11:34 +0700

    push

---
+++ _site/main.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+	//make sure the page is ready befor running the scripts
+	// This is the hamburger menu
+	$(".hamburger").click(function(){
+
+	  $(this).toggleClass('active');
+	  $(".mobile-menu").fadeToggle();
+		});
+
+// put class of exit on elements to make them navigate back to page-nav
+			$(".exit").click(function(){
+
+				$(this).toggleClass('active');
+				$(".page-nav").fadeToggle();
+			});
+
+			// end hamburger menu
+
+			$(document).ready(function(){
+					$('.one-time').slick({
+			dots: true,
+			infinite: true,
+			draggable: true,
+			speed: 1000,
+			slidesToShow: 1,
+			 autoplay: true,
+			adaptiveHeight: true
+			});
+			});
+			});
